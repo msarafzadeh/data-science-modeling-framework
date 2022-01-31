8568dd491796c02e42a46b14ad3da08c014fbe91
Matityahu Sarafzadeh <matthew2001@gmail.com>
2022-01-31 14:02:23 +0200

    label clustering

---
+++ data_seperability.py
@@ -0,0 +1,47 @@
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA, TruncatedSVD
+import matplotlib.patches as mpatches
+
+
+X_reduced_tsne = TSNE(n_components=2, random_state=42).fit_transform(X.values)
+X_reduced_pca = PCA(n_components=2, random_state=42).fit_transform(X.values)
+X_reduced_svd = TruncatedSVD(n_components=2, algorithm='randomized', random_state=42).fit_transform(X.values)
+
+f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(24,6))
+# labels = ['No Fraud', 'Fraud']
+f.suptitle('Clusters using Dimensionality Reduction', fontsize=14)
+
+
+blue_patch = mpatches.Patch(color='#0A0AFF', label='No Fraud')
+red_patch = mpatches.Patch(color='#AF0000', label='Fraud')
+
+
+# t-SNE scatter plot
+ax1.scatter(X_reduced_tsne[:,0], X_reduced_tsne[:,1], c=(y == 0), cmap='coolwarm', label='No Fraud', linewidths=2)
+ax1.scatter(X_reduced_tsne[:,0], X_reduced_tsne[:,1], c=(y == 1), cmap='coolwarm', label='Fraud', linewidths=2)
+ax1.set_title('t-SNE', fontsize=14)
+
+ax1.grid(True)
+
+ax1.legend(handles=[blue_patch, red_patch])
+
+
+# PCA scatter plot
+ax2.scatter(X_reduced_pca[:,0], X_reduced_pca[:,1], c=(y == 0), cmap='coolwarm', label='No Fraud', linewidths=2)
+ax2.scatter(X_reduced_pca[:,0], X_reduced_pca[:,1], c=(y == 1), cmap='coolwarm', label='Fraud', linewidths=2)
+ax2.set_title('PCA', fontsize=14)
+
+ax2.grid(True)
+
+ax2.legend(handles=[blue_patch, red_patch])
+
+# TruncatedSVD scatter plot
+ax3.scatter(X_reduced_svd[:,0], X_reduced_svd[:,1], c=(y == 0), cmap='coolwarm', label='No Fraud', linewidths=2)
+ax3.scatter(X_reduced_svd[:,0], X_reduced_svd[:,1], c=(y == 1), cmap='coolwarm', label='Fraud', linewidths=2)
+ax3.set_title('Truncated SVD', fontsize=14)
+
+ax3.grid(True)
+
+ax3.legend(handles=[blue_patch, red_patch])
+
+plt.show()
